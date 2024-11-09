a4df5aaaf6d6f5b2b9bf8e3ab01079d6df73e2a1
Isaac Guzmán <mizook.21922@gmail.com>
2024-11-08 23:39:28 -0600

    fotos

--- asobi.py
+++ asobi.py
@@ -1,8 +1,8 @@
 # app.py
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-import config
 
+import config
 app = Flask(__name__)
 
 # Conectar a MongoDB
